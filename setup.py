b90d4da4ff65349b6075f3f9286018dd0b9a2b9d
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-02-12 18:07:25 +0600

    Update setup.py
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 ]
 
 setup(
-    name='pynamodb',
+    name='kengine-pynamodb',
     version=__import__('pynamodb').__version__,
     packages=find_packages(exclude=('examples', 'tests', 'typing_tests', 'tests.integration',)),
     url='http://jlafon.io/pynamodb.html',
